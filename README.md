a5d7b7785947c528507964a64dce7bdf7fb64e9a
= <abhishek88666kumar@gmail.com>
2019-12-28 20:09:50 +0530

    Initial Commit

--- README.md
+++ README.md
@@ -1,14 +1,38 @@
 # default_path_provider
 
-A plugin that provide path to the default android directories like Downloads, Pictures, etc.
+[default_path_provider](http://www.github.com/abhidiwakar/default_path_provider) is a flutter plugin that can provide you the path of android's default directories like Downloads,  Pictures, DCIM, etc.
 
-## Getting Started
+```diff
+- Note: This plugin can work only in Android as of now!
+```
 
-This project is a starting point for a Flutter
-[plug-in package](https://flutter.dev/developing-packages/),
-a specialized package that includes platform-specific implementation code for
-Android and/or iOS.
 
-For help getting started with Flutter, view our 
-[online documentation](https://flutter.dev/docs), which offers tutorials, 
-samples, guidance on mobile development, and a full API reference.
+## Usage
+- Add [default_path_provider](https://pub.dartlang.org/packages/default_path_provider#-installing-tab-) as a dependency in your pubspec.yaml file.
+- Handle Read/Write permission in your flutter app. You can use flutter plugins like [permission_handler](https://pub.dev/packages/permission_handler).
+
+```dart
+import 'package:default_path_provider/default_path_provider.dart';
+
+String dirPath = await DefaultPathProvider.getDownloadDirectoryPath;
+```
+
+## Available Methods
+- getDownloadDirectoryPath
+- getPicturesDirectoryPath
+- getDCIMDirectoryPath
+- getNotificationsDirectoryPath
+- getMoviesDirectoryPath
+- getAlarmsDirectoryPath
+- getMusicDirectoryPath
+- getDocumentsDirectoryPath
+- getRingtonesDirectoryPath
+- getPodcastDirectoryPath
+
+## Contributing
+Pull requests are welcome. For major changes, please open an issue first to discuss what you would like to change.
+
+Please make sure to update the tests as appropriate.
+
+## License
+[MIT](https://github.com/abhidiwakar/default_path_provider/blob/master/LICENSE)
--- example/lib/main.dart
+++ example/lib/main.dart
@@ -1,8 +1,10 @@
-import 'package:flutter/material.dart';
-import 'dart:async';
+import 'dart:io';
 
-import 'package:flutter/services.dart';
+import 'package:flutter/material.dart';
+
+import 'package:dio/dio.dart';
 import 'package:default_path_provider/default_path_provider.dart';
+import 'package:permission_handler/permission_handler.dart';
 
 void main() => runApp(MyApp());
 
@@ -12,123 +14,255 @@ class MyApp extends StatefulWidget {
 }
 
 class _MyAppState extends State<MyApp> {
-  String _downloadPath = "Loading...";
-  String _dCIMPath = "Loading...";
-  String _notificationPath = "Loading...";
-  String _moviePath = "Loading...";
-  String _alarmsPath = "Loading...";
-  String _documentsPath = "Loading...";
-  String _musicPath = "Loading...";
-  String _picturesPath = "Loading...";
-  String _podcastPath = "Loading...";
-  String _ringtonePath = "Loading...";
+
+  String _imageURL = "https://images.unsplash.com/photo-1497250681960-ef046c08a56e?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=334&q=80";
+
+  int _progress = 0;
+  bool _isDownloading = false;
+
+
+  _checkPermission() async{
+    PermissionStatus permission = await PermissionHandler().checkPermissionStatus(PermissionGroup.storage);
+    if(permission != PermissionStatus.granted){
+      Map<PermissionGroup, PermissionStatus> permissions = await PermissionHandler().requestPermissions([PermissionGroup.storage]);
+      if(permissions[PermissionGroup.storage] != PermissionStatus.granted){
+        exit(1);
+      }
+    }
+  }
+
+  _startDownload(dir) async{
+    Dio dio = Dio();
+    try {
+      await dio.download(_imageURL, "$dir/myimage.jpeg",
+          onReceiveProgress: (rec, total) {
+            setState(() {
+              _progress = ((rec / total) * 100).toInt();
+              if (_progress == 100) {
+                if(this.mounted){
+                  _isDownloading = false;
+                }
+              }
+            });
+          });
+    } catch (e) {
+      print(e);
+      if(this.mounted){
+        setState(() {
+          _isDownloading = false;
+        });
+      }
+    }
+  }
+
+  _saveToDownloads() async {
+    if(this.mounted){
+      setState(() {
+        _isDownloading = true;
+      });
+    }
+    var dir = await DefaultPathProvider.getDownloadDirectoryPath;
+    _startDownload(dir);
+  }
+
+  _saveToPictures() async {
+    if(this.mounted){
+      setState(() {
+        _isDownloading = true;
+      });
+    }
+    var dir = await DefaultPathProvider.getPicturesDirectoryPath;
+    _startDownload(dir);
+  }
+
+  _saveToDCIM() async {
+    if(this.mounted){
+      setState(() {
+        _isDownloading = true;
+      });
+    }
+    var dir = await DefaultPathProvider.getDCIMDirectoryPath;
+    _startDownload(dir);
+  }
+
+  _saveToNotifications() async {
+    if(this.mounted){
+      setState(() {
+        _isDownloading = true;
+      });
+    }
+    var dir = await DefaultPathProvider.getNotificationsDirectoryPath;
+    _startDownload(dir);
+  }
+
+  _saveToMovies() async {
+    if(this.mounted){
+      setState(() {
+        _isDownloading = true;
+      });
+    }
+    var dir = await DefaultPathProvider.getMoviesDirectoryPath;
+    _startDownload(dir);
+  }
+
+  _saveToAlarms() async {
+    if(this.mounted){
+      setState(() {
+        _isDownloading = true;
+      });
+    }
+    var dir = await DefaultPathProvider.getAlarmsDirectoryPath;
+    _startDownload(dir);
+  }
+
+  _saveToMusic() async {
+    if(this.mounted){
+      setState(() {
+        _isDownloading = true;
+      });
+    }
+    var dir = await DefaultPathProvider.getMusicDirectoryPath;
+    _startDownload(dir);
+  }
+
+  _saveToDocuments() async {
+    if(this.mounted){
+      setState(() {
+        _isDownloading = true;
+      });
+    }
+    var dir = await DefaultPathProvider.getDocumentsDirectoryPath;
+    _startDownload(dir);
+  }
+
+  _saveToRingtone() async {
+    if(this.mounted){
+      setState(() {
+        _isDownloading = true;
+      });
+    }
+    var dir = await DefaultPathProvider.getRingtonesDirectoryPath;
+    _startDownload(dir);
+  }
+
+  _saveToPodcast() async {
+    if(this.mounted){
+      setState(() {
+        _isDownloading = true;
+      });
+    }
+    var dir = await DefaultPathProvider.getPodcastDirectoryPath;
+    _startDownload(dir);
+  }
 
   @override
   void initState() {
     super.initState();
-    _getAllPaths();
-  }
-
-  _getAllPaths() async {
-    try {
-      _downloadPath = await DefaultPathProvider.getDownloadDirectoryPath;
-      _dCIMPath = await DefaultPathProvider.getDCIMDirectoryPath;
-      _notificationPath =
-          await DefaultPathProvider.getNotificationsDirectoryPath;
-      _moviePath = await DefaultPathProvider.getMoviesDirectoryPath;
-      _alarmsPath = await DefaultPathProvider.getAlarmsDirectoryPath;
-      _documentsPath = await DefaultPathProvider.getDocumentsDirectoryPath;
-      _musicPath = await DefaultPathProvider.getMusicDirectoryPath;
-      _picturesPath = await DefaultPathProvider.getPicturesDirectoryPath;
-      _podcastPath = await DefaultPathProvider.getPodcastDirectoryPath;
-      _ringtonePath = await DefaultPathProvider.getRingtonesDirectoryPath;
-    } catch (e) {
-      print(e);
-    }
+    _checkPermission();
   }
 
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
       home: Scaffold(
-        appBar: AppBar(
-          title: const Text('Default Path'),
+          body: SafeArea(
+        child: _isDownloading ? Container(
+          child: Center(
+            child: Column(
+              mainAxisAlignment: MainAxisAlignment.center,
+              children: <Widget>[
+                CircularProgressIndicator(),
+                SizedBox(height: 10.0,),
+                Text("Downloading..."),
+              ],
+            ),
+          ),
+        ) : Container(
+          padding: EdgeInsets.all(10.0),
+          width: double.infinity,
+          child: Column(
+            crossAxisAlignment: CrossAxisAlignment.center,
+            children: <Widget>[
+              Stack(
+                alignment: Alignment.center,
+                children: <Widget>[
+                  CircularProgressIndicator(),
+                  Padding(
+                    padding: EdgeInsets.all(10.0),
+                    child: ClipRRect(
+                      child: Image.network(
+                        _imageURL,
+                        height: 200,
+                        width: double.infinity,
+                        fit: BoxFit.cover,
+                      ),
+                      borderRadius: BorderRadius.circular(10.0),
+                    ),
+                  ),
+                ],
+              ),
+              SizedBox(
+                height: 10.0,
+              ),
+              Expanded(
+                child: ListView(
+                  children: <Widget>[
+                    RaisedButton.icon(
+                      onPressed: () => _saveToDownloads(),
+                      label: Text("Save in Downloads"),
+                      icon: Icon(Icons.file_download),
+                    ),
+                    RaisedButton.icon(
+                      icon: Icon(Icons.camera_alt),
+                      onPressed: () => _saveToDCIM(),
+                      label: Text("Save in DCIM"),
+                    ),
+                    RaisedButton.icon(
+                      icon: Icon(Icons.notifications),
+                      onPressed: () => _saveToNotifications(),
+                      label: Text("Save in Notification"),
+                    ),
+                    RaisedButton.icon(
+                      icon: Icon(Icons.movie),
+                      onPressed: () => _saveToMovies(),
+                      label: Text("Save in Movies"),
+                    ),
+                    RaisedButton.icon(
+                      icon: Icon(Icons.image),
+                      onPressed: () => _saveToPictures(),
+                      label: Text("Save in Pictures"),
+                    ),
+                    RaisedButton.icon(
+                      icon: Icon(Icons.alarm),
+                      onPressed: () => _saveToAlarms(),
+                      label: Text("Save in Alarm"),
+                    ),
+                    RaisedButton.icon(
+                      icon: Icon(Icons.library_music),
+                      onPressed: () => _saveToMusic(),
+                      label: Text("Save in Music"),
+                    ),
+                    RaisedButton.icon(
+                      icon: Icon(Icons.assessment),
+                      onPressed: () => _saveToDocuments(),
+                      label: Text("Save in Document"),
+                    ),
+                    RaisedButton.icon(
+                      icon: Icon(Icons.music_note),
+                      onPressed: () => _saveToRingtone(),
+                      label: Text("Save in Ringtone"),
+                    ),
+                    RaisedButton(
+                      onPressed: () => _saveToPodcast(),
+                      child: Text("Save in Podcast"),
+                    ),
+                  ],
+                ),
+              ),
+            ],
+          ),
         ),
-        body: ListView(
-          children: <Widget>[
-            Card(
-              margin: EdgeInsets.only(left: 8.0, right: 8.0, top: 5.0),
-              child: Container(
-                padding: EdgeInsets.all(5.0),
-                child: Text(_downloadPath),
-              ),
-            ),
-            Card(
-              margin: EdgeInsets.only(left: 8.0, right: 8.0, top: 5.0),
-              child: Container(
-                padding: EdgeInsets.all(5.0),
-                child: Text(_dCIMPath),
-              ),
-            ),
-            Card(
-              margin: EdgeInsets.only(left: 8.0, right: 8.0, top: 5.0),
-              child: Container(
-                padding: EdgeInsets.all(5.0),
-                child: Text(_notificationPath),
-              ),
-            ),
-            Card(
-              margin: EdgeInsets.only(left: 8.0, right: 8.0, top: 5.0),
-              child: Container(
-                padding: EdgeInsets.all(5.0),
-                child: Text(_moviePath),
-              ),
-            ),
-            Card(
-              margin: EdgeInsets.only(left: 8.0, right: 8.0, top: 5.0),
-              child: Container(
-                padding: EdgeInsets.all(5.0),
-                child: Text(_alarmsPath),
-              ),
-            ),
-            Card(
-              margin: EdgeInsets.only(left: 8.0, right: 8.0, top: 5.0),
-              child: Container(
-                padding: EdgeInsets.all(5.0),
-                child: Text(_documentsPath),
-              ),
-            ),
-            Card(
-              margin: EdgeInsets.only(left: 8.0, right: 8.0, top: 5.0),
-              child: Container(
-                padding: EdgeInsets.all(5.0),
-                child: Text(_musicPath),
-              ),
-            ),
-            Card(
-              margin: EdgeInsets.only(left: 8.0, right: 8.0, top: 5.0),
-              child: Container(
-                padding: EdgeInsets.all(5.0),
-                child: Text(_picturesPath),
-              ),
-            ),
-            Card(
-              margin: EdgeInsets.only(left: 8.0, right: 8.0, top: 5.0),
-              child: Container(
-                padding: EdgeInsets.all(5.0),
-                child: Text(_podcastPath),
-              ),
-            ),
-            Card(
-              margin: EdgeInsets.only(left: 8.0, right: 8.0, top: 5.0),
-              child: Container(
-                padding: EdgeInsets.all(5.0),
-                child: Text(_ringtonePath),
-              ),
-            ),
-          ],
-        ),
-      ),
+      )),
     );
   }
 }
